e91f0139b6eb6931657af7a88e7987b8adbc8aba
Adam Kiss <kisssandoradam@gmail.com>
2016-11-29 21:48:48 +0100

    getGalaxies()

--- universe-model/src/main/java/hu/unideb/inf/jaxb/JAXBUtil.java
+++ universe-model/src/main/java/hu/unideb/inf/jaxb/JAXBUtil.java
@@ -1,7 +1,10 @@
 package hu.unideb.inf.jaxb;
 
+import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
@@ -18,15 +21,11 @@ public class JAXBUtil {
 	 * @throws JAXBException on any error
 	 */
 	public static void toXML(Object o, OutputStream os) throws JAXBException {
-		try {
-			JAXBContext	context = JAXBContext.newInstance(o.getClass());
-			Marshaller	marshaller = context.createMarshaller();
-			marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
-			marshaller.setProperty(Marshaller.JAXB_ENCODING, "UTF-8");
-			marshaller.marshal(o, os);
-		} catch(JAXBException e) {
-			throw e;
-		}
+		JAXBContext context = JAXBContext.newInstance(o.getClass());
+		Marshaller marshaller = context.createMarshaller();
+		marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+		marshaller.setProperty(Marshaller.JAXB_ENCODING, "UTF-8");
+		marshaller.marshal(o, os);
 	}
 
 	/**
@@ -34,17 +33,22 @@ public class JAXBUtil {
 	 *
 	 * @param clazz the class of the object
 	 * @param is the {@link InputStream} to read from
-	 * @return the resulting object 
+	 * @return the resulting object
 	 * @throws JAXBException on any error
 	 */
 	public static <T> T fromXML(Class<T> clazz, InputStream is) throws JAXBException {
-		try {
-			JAXBContext	context = JAXBContext.newInstance(clazz);
-			Unmarshaller	unmarshaller = context.createUnmarshaller();
-			return (T) unmarshaller.unmarshal(is);
-		} catch(JAXBException e) {
-			throw e;
-		}
+		JAXBContext context = JAXBContext.newInstance(clazz);
+		Unmarshaller unmarshaller = context.createUnmarshaller();
+		return clazz.cast(unmarshaller.unmarshal(is));
+	}
+	
+	public static <T> T fromXML(Class<T> clazz, String xml) throws JAXBException {
+		return fromXML(clazz, xml, StandardCharsets.UTF_8);
+	}
+	
+	public static <T> T fromXML(Class<T> clazz, String xml, Charset charset) throws JAXBException {
+		InputStream is = new ByteArrayInputStream(xml.getBytes(charset));
+		return fromXML(clazz, is);
 	}
 
 }
--- universe/src/main/java/hu/unideb/inf/universe/main/Application.java
+++ universe/src/main/java/hu/unideb/inf/universe/main/Application.java
@@ -1,14 +1,36 @@
 package hu.unideb.inf.universe.main;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.xml.bind.JAXBException;
 import javax.xml.xquery.XQConnection;
 import javax.xml.xquery.XQDataSource;
 import javax.xml.xquery.XQException;
 import javax.xml.xquery.XQExpression;
 import javax.xml.xquery.XQResultSequence;
 
+import hu.unideb.inf.jaxb.JAXBUtil;
+import hu.unideb.inf.universe.model.Galaxy;
 import net.xqj.basex.BaseXXQDataSource;
 
 public class Application {
+
+	public static void main(String[] args) throws Exception {
+		XQDataSource ds = new BaseXXQDataSource();
+		ds.setProperty("serverName", "localhost");
+		ds.setProperty("port", "1984");
+		ds.setProperty("user", "admin");
+		ds.setProperty("password", "admin");
+
+		XQConnection xqc = ds.getConnection();
+
+		// doSomething(xqc);
+		System.out.println(getGalaxies(xqc));
+		System.out.println("validate: " + validate(xqc));
+
+		xqc.close();
+	}
 
 	public static boolean validate(XQConnection xqc) {
 		try {
@@ -21,22 +43,24 @@ public class Application {
 		}
 	}
 
-	public static void main(String[] args) throws Exception {
-		// String query = "for $x in doc('src/main/resources/fejlett_adatbazis_hazi.xml')//universe return data($x)";
+	public static List<Galaxy> getGalaxies(XQConnection xqc) throws XQException, JAXBException {
+		List<Galaxy> galaxies = new LinkedList<>();
 
-		XQDataSource ds = new BaseXXQDataSource();
-		ds.setProperty("serverName", "localhost");
-		ds.setProperty("port", "1984");
-		ds.setProperty("user", "admin");
-		ds.setProperty("password", "admin");
+		XQExpression xqe = xqc.createExpression();
+		XQResultSequence rs = xqe.executeQuery("for $galaxy in db:open('universe')//galaxies/* return $galaxy");
+		while (rs.next()) {
+			Galaxy galaxy = JAXBUtil.fromXML(Galaxy.class, rs.getItemAsString(null));
+			galaxies.add(galaxy);
+		}
 
-		XQConnection xqc = ds.getConnection();
+		return galaxies;
+	}
+
+	private static void doSomething(XQConnection xqc) throws XQException {
 		XQExpression xqe = xqc.createExpression();
 		XQResultSequence rs = xqe.executeQuery("for $x in db:open('universe')//* return data($x)");
 		rs.writeSequence(System.out, null);
 		System.out.println();
-		System.out.println("validate: " + validate(xqc));
-		xqc.close();
 	}
 
 }
