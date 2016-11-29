28bc556a4897bc54a53510b966930542d9cae3fb
Czuczor Gergő <czuczorgergo@gmail.com>
2016-11-29 20:25:06 +0100

    Added validation.

--- universe/src/main/java/hu/unideb/inf/universe/main/Application.java
+++ universe/src/main/java/hu/unideb/inf/universe/main/Application.java
@@ -1,7 +1,10 @@
 package hu.unideb.inf.universe.main;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.xml.xquery.XQConnection;
 import javax.xml.xquery.XQDataSource;
+import javax.xml.xquery.XQException;
 import javax.xml.xquery.XQExpression;
 import javax.xml.xquery.XQResultSequence;
 
@@ -9,6 +12,16 @@ import net.xqj.basex.BaseXXQDataSource;
 
 public class Application {
 
+	public static boolean validate(XQConnection xqc) {
+		try {
+			XQExpression xqe = xqc.createExpression();
+			XQResultSequence rs = xqe.executeQuery("for $doc in db:open('universe') return validate:xsd($doc, 'C:\\Egyetem\\xmldb\\universe.xsd')");
+			return true;
+		} catch (XQException ex) {
+			return false;
+		}
+	}
+	
 	public static void main(String[] args) throws Exception {
 		// String query = "for $x in doc('src/main/resources/fejlett_adatbazis_hazi.xml')//universe return data($x)";
 
@@ -22,6 +35,7 @@ public class Application {
 		XQExpression xqe = xqc.createExpression();
 		XQResultSequence rs = xqe.executeQuery("for $x in db:open('universe')//* return data($x)");
 		rs.writeSequence(System.out, null);
+		System.out.println(validate(xqc));
 		xqc.close();
 	}
 
