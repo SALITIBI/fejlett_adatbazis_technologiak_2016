cc8aaab30fc2fc1890f517b1358a7ed0923f00fc
Adam Kiss <kisssandoradam@gmail.com>
2016-11-29 20:00:08 +0100

    It works!

--- universe/src/main/java/hu/unideb/inf/universe/main/Application.java
+++ universe/src/main/java/hu/unideb/inf/universe/main/Application.java
@@ -1,11 +1,11 @@
 package hu.unideb.inf.universe.main;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
-import javax.xml.namespace.QName;
 import javax.xml.xquery.XQConnection;
 import javax.xml.xquery.XQDataSource;
-import javax.xml.xquery.XQPreparedExpression;
+import javax.xml.xquery.XQExpression;
 import javax.xml.xquery.XQResultSequence;
 
 import org.basex.core.BaseXException;
@@ -32,33 +32,49 @@ public class Application {
 		// serialize(query);
 		// iterate(query);
 
-		XQDataSource xqs = new BaseXXQDataSource();
-		xqs.setProperty("serverName", "localhost");
-		xqs.setProperty("port", "1984");
-
-		// Change USERNAME and PASSWORD values
-		XQConnection conn = xqs.getConnection("admin", "admin");
-
-		XQPreparedExpression xqpe = conn.prepareExpression("declare variable $x as xs:string external; $x");
-
-		xqpe.bindString(new QName("x"), "Hello World!", null);
-
-		XQResultSequence rs = xqpe.executeQuery();
-
-		while (rs.next()) {
-			System.out.println(rs.getItemAsString(null));
-		}
+		XQDataSource ds = new BaseXXQDataSource();
+		ds.setProperty("serverName", "localhost");
+		ds.setProperty("port", "1984");
+		ds.setProperty("user", "admin");
+		ds.setProperty("password", "admin");
 		
-		xqpe = conn.prepareExpression("for $x in //universe return data($x)");
-		rs = xqpe.executeQuery();
-		while (rs.next()) {
-			System.out.println(rs.getItemAsString(null));
-		}
+		XQConnection xqc = ds.getConnection();
+		XQExpression xqe = xqc.createExpression();
+		XQResultSequence rs = xqe.executeQuery("for $x in db:open('universe')//* return data($x)");
+		rs.writeSequence(System.out, null);
+		xqc.close();
 		
-		
-		System.out.println("Conn isClosed: " + conn.isClosed());
-		conn.close();
-		System.out.println("Conn isClosed: " + conn.isClosed());
+//		ds.setLogWriter(new PrintWriter(System.out));
+//
+//		// Change USERNAME and PASSWORD values
+//		XQConnection conn = ds.getConnection("admin", "admin");
+////
+////		XQPreparedExpression xqpe = conn.prepareExpression("declare variable $x as xs:string external; $x");
+////
+////		xqpe.bindString(new QName("x"), "Hello World!", null);
+////
+////		XQResultSequence rs = xqpe.executeQuery();
+////
+////		while (rs.next()) {
+////			System.out.println(rs.getItemAsString(null));
+////		}
+////		
+////		XQExpression createExpression = conn.createExpression();
+////		XQResultSequence executeQuery = createExpression.executeQuery("for $x in //galaxy return data($x)");
+////		executeQuery.writeSequence(System.out, null);
+////		
+////		System.out.println("Conn isClosed: " + conn.isClosed());
+////		conn.close();
+////		System.out.println("Conn isClosed: " + conn.isClosed());
+//		
+//		
+////		XQConnection xqc = ds.getConnection();
+//	    XQExpression xqe = conn.createExpression();
+//
+////	    xqe.executeCommand("SHOW DATABASES");
+//	    xqe.executeCommand("SHOW SESSIONS");
+//	    xqe.executeCommand("SHOW USERS");
+//	    xqe.executeCommand("SHOW EVENTS");
 	}
 
 	static void query(final String query) throws BaseXException {
