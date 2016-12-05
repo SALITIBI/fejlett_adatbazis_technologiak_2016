b50750cd3208e1fa6c188bf56c6e6525491a8cfa
Adam Kiss <kisssandoradam@gmail.com>
2016-12-05 16:21:23 +0100

    gui with xml update

--- universe/src/main/java/hu/unideb/inf/universe/main/Application.java
+++ universe/src/main/java/hu/unideb/inf/universe/main/Application.java
@@ -2,12 +2,10 @@ package hu.unideb.inf.universe.main;
 
 import java.awt.BorderLayout;
 import java.awt.Component;
-import java.awt.Dialog;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
-import java.awt.Graphics;
-import java.awt.Insets;
-import java.util.List;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 
 import javax.swing.Box;
 import javax.swing.BoxLayout;
@@ -16,15 +14,13 @@ import javax.swing.JComboBox;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JList;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
-import javax.swing.ListCellRenderer;
-import javax.swing.border.Border;
 import javax.swing.border.EmptyBorder;
 import javax.xml.xquery.XQConnection;
+import javax.xml.xquery.XQException;
 
 import hu.unideb.inf.universe.connection.ConnectionUtil;
 import hu.unideb.inf.universe.model.Comet;
@@ -32,7 +28,6 @@ import hu.unideb.inf.universe.model.Galaxy;
 import hu.unideb.inf.universe.model.Mineral;
 import hu.unideb.inf.universe.model.Moon;
 import hu.unideb.inf.universe.model.Planet;
-import hu.unideb.inf.universe.model.Property;
 import hu.unideb.inf.universe.model.SolarSystem;
 import hu.unideb.inf.universe.model.Star;
 import hu.unideb.inf.universe.service.UniverseService;
@@ -47,6 +42,7 @@ public class Application {
 		JFrame frame = new JFrame("Fejlett XML Technológiák beadandó 2016");
 
 		JMenu actionsMenu = new JMenu("Actions");
+		actionsMenu.setEnabled(false);
 		// actionsMenu.add(new JMenuItem("Add new galaxy"));
 		// actionsMenu.add(new JMenuItem("Add new solar system"));
 		// actionsMenu.add(new JMenuItem("Add new planet"));
@@ -276,102 +272,40 @@ public class Application {
 		frame.setPreferredSize(new Dimension(1280, 720));
 		frame.pack();
 		frame.setVisible(true);
+		frame.addWindowListener(new WindowListener() {
+			@Override
+			public void windowOpened(WindowEvent e) {
+			}
 
-		// System.out.println("validate: " + us.validate());
-		//
-		// List<Galaxy> galaxies = us.findAllGalaxies();
-		// System.out.println("galaxies: " + galaxies);
-		//
-		// List<SolarSystem> solarSystems = us.findAllSolarSystemsInGalaxy(galaxies.get(0));
-		// System.out.println("solar system: " + solarSystems);
-		//
-		// Star star = us.findStarInSolarSystem(solarSystems.get(0));
-		// System.out.println("star: " + star);
-		//
-		// List<Planet> planets = us.findAllPlanetsInSolarSystem(solarSystems.get(0));
-		// System.out.println("planets: " + planets);
-		//
-		// List<Comet> comets = us.findAllCometsInSolarSystem(solarSystems.get(0));
-		// System.out.println("comets: " + comets);
-		//
-		// List<Moon> moons = us.findAllMoonsAroundPlanet(planets.get(0));
-		// System.out.println("moons: " + moons);
-		//
-		// List<Mineral> minerals = us.findAllMineralsInComet(comets.get(0));
-		// System.out.println("minerals: " + minerals);
-		//
-		// Property newQuantity = new Property("kg", 555555.55);
-		// us.updateMineralOnComet(comets.get(0).getName(), minerals.get(0).getElementName(), newQuantity);
-		//
-		// Property quantity = new Property("g", 1024.1024);
-		// us.addMineralToComet(comets.get(0).getName(), "Element Zero", quantity);
-		//
-		// minerals = us.findAllMineralsInComet(comets.get(0));
-		// System.out.println("minerals: " + minerals);
-		//
-		// Property newOrbitalPeriodProperty = new Property("day", 7.0);
-		// us.updateCometOrbitalPeriod(comets.get(0).getName(), newOrbitalPeriodProperty);
-		//
-		// Property orbitalPeriod = new Property("year", 2520);
-		// us.addCometToSolarSystem(solarSystems.get(0).getName(), "Hale–Bopp", orbitalPeriod);
-		//
-		// Property radius = new Property("km", 2048.1024);
-		// us.addMoonToPlanet(planets.get(0).getName(), "Moon-Moon", radius);
-		//
-		// us.addPlanetToSolarSystem(solarSystems.get(0).getName(), "Mercury",
-		// new Property("km", 2439.7),
-		// new Property("day", 87.969),
-		// new Property("kps", 47.3621),
-		// new Property(null, 0.205630),
-		// new Property("AU", 0.387098),
-		// new Property("kg", 3.3011 * Math.pow(10, 23)));
-		//
-		// planets = us.findAllPlanetsInSolarSystem(solarSystems.get(0));
-		// System.out.println("planets: " + planets);
-		//
-		// moons = us.findAllMoonsAroundPlanet(planets.get(0));
-		// System.out.println("moons: " + moons);
-		//
-		// comets = us.findAllCometsInSolarSystem(solarSystems.get(0));
-		// System.out.println("comets: " + comets);
-		//
-		// System.out.println("Deleting mineral: " + minerals.get(0) + " on comet: " + comets.get(0));
-		// us.deleteMineralOnComet(comets.get(0).getName(), minerals.get(0).getElementName());
-		//
-		// minerals = us.findAllMineralsInComet(comets.get(0));
-		// System.out.println("minerals: " + minerals);
-		//
-		// System.out.println("Deleting comet: " + comets.get(0));
-		// us.deleteComet(comets.get(0).getName());
-		//
-		// comets = us.findAllCometsInSolarSystem(solarSystems.get(0));
-		// System.out.println("comets: " + comets);
-		//
-		// System.out.println("Deleting moon: " + moons.get(0));
-		// us.deleteMoon(moons.get(0).getName());
-		//
-		// moons = us.findAllMoonsAroundPlanet(planets.get(0));
-		// System.out.println("moons: " + moons);
-		//
-		// System.out.println("Deleting planet: " + planets.get(0));
-		// us.deletePlanet(planets.get(0).getName());
-		//
-		// planets = us.findAllPlanetsInSolarSystem(solarSystems.get(0));
-		// System.out.println("planets: " + planets);
-		//
-		// System.out.println("Deleting solar system: " + solarSystems.get(0));
-		// us.deleteSolarSystem(solarSystems.get(0).getName());
-		//
-		// solarSystems = us.findAllSolarSystemsInGalaxy(galaxies.get(0));
-		// System.out.println("solar system: " + solarSystems);
-		//
-		// System.out.println("Deleting galaxy: " + galaxies.get(0));
-		// us.deleteGalaxy(galaxies.get(0).getName());
-		//
-		// galaxies = us.findAllGalaxies();
-		// System.out.println("galaxies: " + galaxies);
+			@Override
+			public void windowIconified(WindowEvent e) {
+			}
 
-		xqc.close();
+			@Override
+			public void windowDeiconified(WindowEvent e) {
+			}
+
+			@Override
+			public void windowDeactivated(WindowEvent e) {
+			}
+
+			@Override
+			public void windowClosing(WindowEvent e) {
+				try {
+					xqc.close();
+				} catch (XQException ex) {
+					ex.printStackTrace();
+				}
+			}
+
+			@Override
+			public void windowClosed(WindowEvent e) {
+			}
+
+			@Override
+			public void windowActivated(WindowEvent e) {
+			}
+		});
 	}
 
 }
