b1ad8aa36798e3c38007e62c88d421be5ec07a58
Czuczor Gergő <czuczorgergo@gmail.com>
2016-12-06 19:34:41 +0100

    Added unit conversion to findSmallPlanetsGroupedBySolarSystem.

--- universe/src/main/java/hu/unideb/inf/universe/main/Application.java
+++ universe/src/main/java/hu/unideb/inf/universe/main/Application.java
@@ -101,9 +101,7 @@ public class Application {
 		Moon[] moons = us.findAllMoonsAroundPlanet(planets[0]).toArray(new Moon[0]);
 		Comet[] comets = us.findAllCometsInSolarSystem(solarSystems[0]).toArray(new Comet[0]);
 		Mineral[] minerals = us.findAllMineralsInComet(comets[0]).toArray(new Mineral[0]);
-		
-		System.out.println(us.avgOrbitalSpeedOfPlanetsThatHaveMoonsWithRadiusBetween(137, 138));
-		
+				
 		galaxiesComboBox = new JComboBox<>(galaxies);
 		galaxiesComboBox.setAlignmentX(Component.CENTER_ALIGNMENT);
 		galaxiesComboBox.setRenderer(new DefaultListCellRenderer() {
